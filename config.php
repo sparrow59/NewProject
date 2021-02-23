ec6882b8d7b6d5d24d15d4fead86e02d6ea1d01f
sparrow59 <68141563+sparrow59@users.noreply.github.com>
2021-02-23 20:59:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "lanesra1886", "baca_buku");
+$conn = new mysqli("localhost", "root", "lanesrA1886", "baca_buku");
 
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
